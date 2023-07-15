5a1d382a96a6517b042eb05a9bc650d345599fa9
DESKTOP-91J784L\user <naum.cristi@yahoo.com>
2023-07-15 10:10:35 +0300

    Upload 1

---
+++ src/main/java/com/tfp/examenrefacere/ExamenRefacereApplication.java
@@ -0,0 +1,13 @@
+package com.tfp.examenrefacere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenRefacereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenRefacereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfp/examenrefacere/ExamenRefacereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfp.examenrefacere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenRefacereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
